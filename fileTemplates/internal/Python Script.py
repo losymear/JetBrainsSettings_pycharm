08dff05c235876ec78db2b62e60c044ee5fddd03
losymear <w15868821768@gmail.com>
2018-09-27 20:12:12 +0800

    PY-2018.2.3 <losymear@wangyindembp Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, _@user_Default.icls, project.default.xml, rainbow_brackets.xml, git.xml, other.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, Python Script.py, plugin_ui.xml, colors.scheme.xml, _@user_Darcula.icls, databaseDrivers.xml, jdk.table.xml, laf.xml, material_theme.xml, proxy.settings.xml, editor.codeinsight.xml, databaseSettings.xml, keymap.xml, debugger.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Filename : ${NAME}
+# @Date : ${YEAR}-${MONTH}-${DAY}-${HOUR}-${MINUTE}
+# @Project: ${PROJECT_NAME}
+# @AUTHOR : ${USER}
+
+
+if __name__ == '__main__':
+    pass
